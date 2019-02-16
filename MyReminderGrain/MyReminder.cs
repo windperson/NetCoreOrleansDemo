96839ee58b757bcfa67f3c48f28d8b72412675d3
Chen Yu Pao <windperson@gmail.com>
2019-02-16 15:59:34 +0800

    Add some comment

--- MyReminderGrain/MyReminder.cs
+++ MyReminderGrain/MyReminder.cs
@@ -11,7 +11,7 @@ namespace MyReminderGrain
     {
         private readonly IOutputMsg _outputMsg;
 
-        private Dictionary<string, ReminderInfo> _registeredReminders = new Dictionary<string, ReminderInfo>();
+        private readonly Dictionary<string, ReminderInfo> _registeredReminders = new Dictionary<string, ReminderInfo>();
 
         private const int UpperLimit = 0;
         private int _calledTimes;
@@ -27,6 +27,9 @@ namespace MyReminderGrain
             var reminderName = $"myReminder{_calledTimes}";
             var reminder = await RegisterOrUpdateReminder(reminderName, TimeSpan.FromSeconds(30), TimeSpan.FromMinutes(1));
             _registeredReminders[reminderName] = new ReminderInfo { Reminder = reminder, CalledCount = 1 };
+
+            //TODO: init some long-running Task job.
+
             var output = new HelloMyValue { Greeting = "Hello World!", YellTime = DateTime.Now };
             _outputMsg.Output(output.Greeting);
 
